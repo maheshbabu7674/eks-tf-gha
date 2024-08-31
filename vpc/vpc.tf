01641114ce9b4991cf0f40c1888685052bbb11c3
maheshbabu7674 <112943607+maheshbabu7674@users.noreply.github.com>
2024-08-31 13:10:48 +0530

    Update vpc.tf

--- vpc/vpc.tf
+++ vpc/vpc.tf
@@ -1,9 +1,5 @@
 resource "aws_vpc" "eks_vpc" {
   cidr_block = var.vpc_cidr
-
-  tags = {
-    Name = "${var.cluster_name}-vpc"
-  }
 }
 
 resource "aws_subnet" "public_subnet" {
@@ -11,16 +7,8 @@ resource "aws_subnet" "public_subnet" {
   vpc_id                  = aws_vpc.eks_vpc.id
   cidr_block              = element(var.public_subnets, count.index)
   map_public_ip_on_launch = true
-
-  tags = {
-    Name = "${var.cluster_name}-public-subnet-${count.index}"
-  }
 }
 
 resource "aws_internet_gateway" "igw" {
   vpc_id = aws_vpc.eks_vpc.id
-
-  tags = {
-    Name = "${var.cluster_name}-igw"
-  }
 }
