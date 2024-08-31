123207942b91e45dd0e4b27d3bbe0e1273ecca97
maheshbabu7674 <112943607+maheshbabu7674@users.noreply.github.com>
2024-08-31 13:18:06 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -30,12 +30,11 @@ provider "aws" {
   version = "~> 3.0"
 }
 
-# VPC Configuration
-module "vpc" {
-  source = "./vpc"
 
-  vpc_cidr       = var.vpc_cidr
-  public_subnets = var.public_subnets
+
+# EKS Cluster Configuration
+data "aws_vpc" "default" {
+  default = true
 }
 
 # EKS Cluster Configuration
@@ -46,19 +45,6 @@ module "eks" {
   instance_type    = var.instance_type
   desired_capacity = var.desired_capacity
 
-  vpc_id         = module.vpc.vpc_id
-  public_subnets = module.vpc.public_subnets
-}
-
-# Outputs
-output "eks_cluster_name" {
-  value = module.eks.cluster_name
-}
-
-output "eks_cluster_endpoint" {
-  value = module.eks.cluster_endpoint
-}
-
-output "eks_cluster_arn" {
-  value = module.eks.cluster_arn
+  vpc_id           = data.aws_vpc.default.id
+  public_subnets   = data.aws_vpc.default.public_subnet_ids
 }
